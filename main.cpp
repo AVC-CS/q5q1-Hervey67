fa23ebfa99cc3623ecddca81c4ede508a3d7e9aa
Hervey67 <herv2007@gmail.com>
2026-04-13 13:08:03 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,25 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n, m;
+    char letter;
     // TODO: ask user for input
+    cin >> n;
+    m = n;
 
     // TODO: outer loop for each row
+    for (int i = 0; i <= n - 1; i++){
+        for (int j = n - 1 - i; j <= n -1; j++){
+            letter = 65;
+            if (m == n){
+                for (int k = n; k <= n; k++){
+                letter = 65 + 1 ;
+                }
+            }
+            cout << letter << " ";
+        }
+        cout << endl; 
+    }
 
         // TODO: inner loop to print letters for this row
 
